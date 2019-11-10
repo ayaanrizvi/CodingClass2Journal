a7b2a4e6ada78c3f84941612a569caa01990bd11
ayaanrizvi <56722457+ayaanrizvi@users.noreply.github.com>
2019-11-10 15:41:57 -0800

    Create Ayaan_MadLib2.py

---
+++ Class2/Ayaan_MadLib2.py
@@ -0,0 +1,8 @@
+#Ayaan_Madlib2.py
+adverb = input('Please enter an adverb: ')
+noun = input('Please enter a noun: ')
+verb = input('Please enter a verb ending in -ed: ')
+animal = input('Please enter an animal: ')
+print('Your MadLib:')
+print('The', adverb, noun, verb, 'over the lazy brown', animal + '.')
+input()
